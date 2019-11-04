cc0915be2f270fd529760a0d696928f15d9063df
Jean-Francois Bouzereau <jfbouzereau@netcourrier.com>
2019-11-04 08:39:43 +0100

    Allow simultaneous games

--- server.js
+++ server.js
@@ -10,22 +10,16 @@ var app = express();
 var server = http.createServer(app);
 var wss = new WebSocketServer( {server:server} );
 
-var cndesktop = null;
-var cnmobiles = [];
-var cnid = 0;
+var cndesktop = {};
+var cnmobiles = {};
+var playerid = 0;
 
 
 //********************************************************
 
-app.get("/", function(req,res) {
-	var ua = req.headers["user-agent"];	
-	var mobile = !!ua.match(/(Android)|(iPhone)|(iPad)/);
+app.get("/",init);
 
-	if(mobile)
-		res.sendFile("mobile.html",{root:"static"});
-	else
-		res.sendFile("desktop.html",{root:"static"});
-});
+app.get("/:gid([a-z][a-z][a-z])", init);
 
 app.get("/:filename", function(req,res) {
 	res.sendFile(req.params.filename,{root:"static"});
@@ -42,6 +36,23 @@ process.on("SIGINT", terminate);
 
 //********************************************************
 
+function init(req,res) {
+
+	var ua = req.headers["user-agent"];	
+	var mobile = !!ua.match(/(Android)|(iPhone)|(iPad)/);
+
+	if(req.url.indexOf("desktop")>=0)
+		res.sendFile("desktop.html",{root:"static"});
+	else if(req.url.indexOf("mobile")>=0)
+		res.sendFile("mobile.html",{root:"static"});
+	else if(mobile)
+		res.sendFile("mobile.html",{root:"static"});
+	else
+		res.sendFile("desktop.html",{root:"static"});
+}
+
+//********************************************************
+
 wss.on("connection", function(cn) {
 	
     start_connection(cn);
@@ -49,42 +60,48 @@ wss.on("connection", function(cn) {
     cn.on("message", function(data) {
         var msg = JSON.parse(data);
 		console.log("MESSAGE "+data);
+		
         switch(msg.type) {
 			case "desktop":
-				cndesktop = cn;
-				// close all mobiles already connected
-				broadcast({type:"close"});
-				cnmobiles = [];
+				var gid = random_gid();	// game id
+				cn.gid = gid;
+				cndesktop[gid] = cn;
+				cnmobiles[gid] = [];
+				forward_desktop(gid,{type:"gid",value:gid});
 				break;
 
 			case "mobile":
-				cn.id = ++cnid;
-				cnmobiles.push(cn);
+				if(!msg.gid) break;
+				var gid = msg.gid;
+				if(!cndesktop[gid]) break;
+				if(!cnmobiles[gid]) break;
+				cnmobiles[gid].push(cn);
 
-				msg.id = cn.id;
-				forward_desktop(msg);
+				cn.gid = gid;
+				cn.pid = ++playerid;
+				forward_desktop(gid,{type:"mobile",pid:cn.pid});
 				break;
 
 			case "welcome":
-				forward_mobile(msg);
+				forward_mobile(cn.gid,msg);
 				break;
 
             case "user":
-				msg.id = cn.id;
-				forward_desktop(msg);
+				msg.pid = cn.pid;
+				forward_desktop(cn.gid,msg);
 				break;
 
 			case "question":
-				broadcast(msg);
+				broadcast_mobiles(cn.gid,msg);
 				break;
 
 			case "answer":
-				msg.id = cn.id;
-				forward_desktop(msg);
+				msg.pid = cn.pid;
+				forward_desktop(cn.gid,msg);
 				break;
 
 			case "status":
-				forward_mobile(msg);
+				forward_mobile(cn.gid,msg);
 				break;
             }
         });
@@ -92,36 +109,83 @@ wss.on("connection", function(cn) {
     cn.on("close", close_connection);
     });
 
+//********************************************************
+
 function start_connection(cn) {
 	// nothing so far
 }
 
+//********************************************************
+
 function close_connection() {
+
+	var gid = this.gid;
+	if(cndesktop[gid] && cndesktop[gid]==this) {
+		console.log("REMOVING DESKTOP "+gid);
+		broadcast_mobiles(gid,{type:"close"});
+		delete cndesktop[gid];
+	} 
+	else if(cnmobiles[gid]) {
+		for(var i=0;i<cnmobiles[gid].length;i++)
+			if(cnmobiles[gid][i]==this) {
+				console.log("REMOVING MOBILE "+gid+" "+i);
+				cnmobiles[gid].splice(i,1);
+			}
+	}
+
 }
 
-function forward_desktop(msg) {
-	if(cndesktop)
-		try { cndesktop.send(JSON.stringify(msg)); }
+//********************************************************
+
+function forward_desktop(gid,msg) {
+	if(cndesktop[gid])
+		try { cndesktop[gid].send(JSON.stringify(msg)); }
 		catch(err) {}
 }
 
-function forward_mobile(msg) {
-	for(var i=0;i<cnmobiles.length;i++)
-		if(cnmobiles[i].id==msg.id)
-			try { cnmobiles[i].send(JSON.stringify(msg)); }
+//********************************************************
+
+function forward_mobile(gid,msg) {
+	if(!cnmobiles[gid]) return;
+	for(var i=0;i<cnmobiles[gid].length;i++)
+		if(cnmobiles[gid][i].pid==msg.pid)
+			try { cnmobiles[gid][i].send(JSON.stringify(msg)); }
 			catch(err) {}
 }
 
-function broadcast(msg) {
-	for(var i=0;i<cnmobiles.length;i++)
-		try { cnmobiles[i].send(JSON.stringify(msg)); }
-		catch(err) {}
+//********************************************************
+
+function broadcast_mobiles(gid,msg) {
+	if(cnmobiles[gid])
+		for(var i=0;i<cnmobiles[gid].length;i++)
+			try { cnmobiles[gid][i].send(JSON.stringify(msg)); }
+			catch(err) {}
 }
 
+//********************************************************
+
 function terminate() {
-	forward_desktop({type:"close"});	
-	broadcast({type:"close"});
+
+	for(var gid in cndesktop) 
+		forward_desktop(gid,{type:"close"});	
+
+	for(var gid in cnmobiles)
+		broadcast_mobiles(gid,{type:"close"});
+
 	process.exit(0);
 }
 
+//********************************************************
+
+function random_gid() {
+	const alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var gid = "";
+	for(var i=0;i<3;i++)
+		gid += alphabet[(Math.random()*alphabet.length)|0];
+	return gid;
+}
+
+//********************************************************
+
+	
 
