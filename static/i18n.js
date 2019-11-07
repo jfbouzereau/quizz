4657e840e265f65302af6e5c5939da3af8eeb5f6
Jean-Francois Bouzereau <jfbouzereau@netcourrier.com>
2019-11-07 12:19:08 +0100

    Can shuffle the colors

--- static/i18n.js
+++ static/i18n.js
@@ -1,13 +1,17 @@
-var I_GAMES = "Questionnaires";
-var I_WELCOME = "Bienvenue % !";
-var I_TEAM = "Nom de l'équipe";
-var I_ANSWER = "Vous avez répondu %";
-var I_NANSWER = "Vous n'avez pas répondu";
-var I_USCORE = "Score %";
-var I_SCORE = "SCORE";
-var I_FSCORE = "SCORE FINAL";
-var I_THANKS = "Merci";
-var I_WAIT = "Attente de la question %...";
+var I_GAMES = "Questionnaires";					// games
+var I_WELCOME = "Bienvenue % !";				// welcome
+var I_TEAM = "Nom de l'équipe";					// name of your team
+var I_ANSWER = "Vous avez répondu %";			// you answered
+var I_NANSWER = "Vous n'avez pas répondu";		// you didnt answer
+var I_USCORE = "Score %";						// score
+var I_SCORE = "SCORE";							// score
+var I_FSCORE = "SCORE FINAL";					// final score
+var I_THANKS = "Merci";							// thank you
+var I_WAIT = "Attente de la question %...";		// waiting for question
+var I_SHOW = "Montrer les réponses";			// show the answers
+var I_SETUP = "Réglages";						// setup
+var I_DELAY = "% secondes";						//
+var I_COLOR = "Mélanger les couleurs";			// shuffle the colors
 
 function i18n(msg,param) {
 	return msg.replace(/[%]/,param);
