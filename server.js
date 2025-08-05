0ee222f2c5b982316f877a8798d5aa234463d2b1
Jean-Francois Bouzereau <jfbouzereau@netcourrier.com>
2025-08-05 12:20:13 +0200

    Add files via upload
    Fix various bugs

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
-
-var http = require("http");
+var fs = require("fs");
+var https = require("https");
 var express = require("express");
 var WebSocketServer = require("ws").Server;
 var spawn = require("child_process").spawn;
 
 const port = 8000;
 
+const options = {
+  key: fs.readFileSync('../privkey.pem'),
+  cert: fs.readFileSync('../cert.pem'),
+  ca: fs.readFileSync('../chain.pem')
+};
+
 var app = express();
-var server = http.createServer(app);
+var server = https.createServer(options,app);
 var wss = new WebSocketServer( {server:server} );
 
-var cndesktop = {};
-var cnmobiles = {};
-var playerid = 0;
+var cndesktop = null;
+var cnmobiles = [];
 
+var playerid = 0;
 
 //********************************************************
 
+app.use(function (req, res, next) {
+	var d = (new Date())+"";
+	d = d.replace(/GMT.*/,"");
+  console.log(d+"\t"+req.connection.remoteAddress+"\t"+req.url);
+  next();
+});
+
 app.get("/",init);
 
-app.get("/:gid([a-z][a-z][a-z])", init);
+app.get("/getstate", function(req,res) {
+	res.send(cndesktop?"Busy":"Free");
+});
 
 app.get("/:filename", function(req,res) {
 	res.sendFile(req.params.filename,{root:"static"});
@@ -38,71 +53,74 @@ process.on("SIGINT", terminate);
 
 function init(req,res) {
 
-	var ua = req.headers["user-agent"];	
-	var mobile = !!ua.match(/(Android)|(iPhone)|(iPad)/);
 
 	if(req.url.indexOf("desktop")>=0)
 		res.sendFile("desktop.html",{root:"static"});
-	else if(req.url.indexOf("mobile")>=0)
+	else 
 		res.sendFile("mobile.html",{root:"static"});
-	else if(mobile)
-		res.sendFile("mobile.html",{root:"static"});
-	else
-		res.sendFile("desktop.html",{root:"static"});
 }
 
 //********************************************************
 
-wss.on("connection", function(cn) {
+wss.on("connection", function(cn, req) {
 	
     start_connection(cn);
 
     cn.on("message", function(data) {
         var msg = JSON.parse(data);
-		console.log("MESSAGE "+data);
+	console.log("MESSAGE "+data);
 		
         switch(msg.type) {
-			case "desktop":
-				var gid = random_gid();	// game id
-				cn.gid = gid;
-				cndesktop[gid] = cn;
-				cnmobiles[gid] = [];
-				forward_desktop(gid,{type:"gid",value:gid});
-				break;
+	case "desktop":
 
-			case "mobile":
-				if(!msg.gid) break;
-				var gid = msg.gid;
-				if(!cndesktop[gid]) break;
-				if(!cnmobiles[gid]) break;
-				cnmobiles[gid].push(cn);
+		broadcast_mobiles({type:"close"});
 
-				cn.gid = gid;
-				cn.pid = ++playerid;
-				forward_desktop(gid,{type:"mobile",pid:cn.pid});
-				break;
+		cndesktop = cn;
+		cnmobiles = [];
+		forward_desktop({type:"gid",value:0});
+		break;
 
-			case "welcome":
-				forward_mobile(cn.gid,msg);
-				break;
+	case "mobile":
+		if(!cndesktop)  {
+			var reply = {type:"error",value:"nogame"};
+			try {cn.send(JSON.stringify(reply));} catch(err){};
+			return;
+		}
 
-            case "user":
-				msg.pid = cn.pid;
-				forward_desktop(cn.gid,msg);
-				break;
+		cnmobiles.push(cn);
 
-			case "question":
-				broadcast_mobiles(cn.gid,msg);
-				break;
+		playerid++;
+		cn.pid = playerid;
+		console.log("CN.PID = "+cn.pid);
+		forward_desktop({type:"mobile",pid:cn.pid});
+		break;
 
-			case "answer":
-				msg.pid = cn.pid;
-				forward_desktop(cn.gid,msg);
-				break;
+	case "welcome":
+		forward_mobile(msg);
+		break;
 
-			case "status":
-				forward_mobile(cn.gid,msg);
-				break;
+	case "user":
+		msg.pid = cn.pid;
+		forward_desktop(msg);
+		break;
+
+	case "question":
+		broadcast_mobiles(msg);
+		break;
+
+	case "answer":
+		msg.pid = cn.pid;
+		forward_desktop(msg);
+		break;
+
+	case "retry" :
+		msg.type = "question";
+		forward_mobile(msg);
+		break;
+
+	case "status":
+		forward_mobile(msg);
+		break;
             }
         });
 
@@ -119,73 +137,70 @@ function start_connection(cn) {
 
 function close_connection() {
 
-	var gid = this.gid;
-	if(cndesktop[gid] && cndesktop[gid]==this) {
-		console.log("REMOVING DESKTOP "+gid);
-		broadcast_mobiles(gid,{type:"close"});
-		delete cndesktop[gid];
+	if(cndesktop && cndesktop==this) {
+		console.log("REMOVING DESKTOP ");
+		broadcast_mobiles({type:"close"});
+		cndesktop = null;
 	} 
-	else if(cnmobiles[gid]) {
-		for(var i=0;i<cnmobiles[gid].length;i++)
-			if(cnmobiles[gid][i]==this) {
-				console.log("REMOVING MOBILE "+gid+" "+i);
-				cnmobiles[gid].splice(i,1);
+	else if(cnmobiles) {		
+		/*
+		for(var i=0;i<cnmobiles.length;i++)
+			if(cnmobiles[i]==this) {
+				console.log("REMOVING MOBILE "+i);
+				cnmobiles.splice(i,1);
 			}
+		*/
 	}
 
 }
 
 //********************************************************
 
-function forward_desktop(gid,msg) {
-	if(cndesktop[gid])
-		try { cndesktop[gid].send(JSON.stringify(msg)); }
-		catch(err) {}
+function forward_desktop(msg) {
+	if(cndesktop)
+	try { cndesktop.send(JSON.stringify(msg)); }
+	catch(err) {}
 }
 
 //********************************************************
 
-function forward_mobile(gid,msg) {
-	if(!cnmobiles[gid]) return;
-	for(var i=0;i<cnmobiles[gid].length;i++)
-		if(cnmobiles[gid][i].pid==msg.pid)
-			try { cnmobiles[gid][i].send(JSON.stringify(msg)); }
-			catch(err) {}
+function forward_mobile(msg) {
+	if(!cnmobiles) return;
+	for(var i=0;i<cnmobiles.length;i++)
+		if(cnmobiles[i].pid==msg.pid)
+			try { cnmobiles[i].send(JSON.stringify(msg)); }
+			catch(err) { console.log(err); }
 }
 
 //********************************************************
 
-function broadcast_mobiles(gid,msg) {
-	if(cnmobiles[gid])
-		for(var i=0;i<cnmobiles[gid].length;i++)
-			try { cnmobiles[gid][i].send(JSON.stringify(msg)); }
-			catch(err) {}
+function broadcast_mobiles(msg) {
+	if(!cnmobiles) return;
+
+	var index = -1;
+	f();
+
+	function f() {
+		index++;
+		if(index>=cnmobiles.length) return;
+		var cn = cnmobiles[index];
+		try { cn.send(JSON.stringify(msg)); }
+		catch(err) { console.log(err); }		
+		setTimeout(f,50);
+	}
+
 }
 
 //********************************************************
 
 function terminate() {
 
-	for(var gid in cndesktop) 
-		forward_desktop(gid,{type:"close"});	
+	forward_desktop({type:"close"});	
 
-	for(var gid in cnmobiles)
-		broadcast_mobiles(gid,{type:"close"});
+	broadcast_mobiles({type:"close"});
 
 	process.exit(0);
 }
 
 //********************************************************
-
-function random_gid() {
-	const alphabet = "abcdefghijklmnopqrstuvwxyz";
-	var gid = "";
-	for(var i=0;i<3;i++)
-		gid += alphabet[(Math.random()*alphabet.length)|0];
-	return gid;
-}
-
-//********************************************************
-
-	
 
